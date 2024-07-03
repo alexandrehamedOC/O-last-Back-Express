e701be0a87036affcf1c5b466abab0601a90fa52
JulienMedina <julien.medina16@gmail.com>
2024-07-03 21:52:50 +0200

    feat: Add route /api/v1/users/login

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { UserDatamapper } from "../datamappers/index.datamapper.js";
+import { ProfilDatamapper, UserDatamapper, PostDatamapper, RateDatamapper } from "../datamappers/index.datamapper.js";
 import CoreController from "./core.controller.js";
 import ApiError from "../errors/api.errors.js";
 import jwt from 'jsonwebtoken';
@@ -18,5 +18,36 @@ export default class UserController extends CoreController{
 
     const token = jwt.sign({email} , process.env.TOKEN_SECRET, { expiresIn: '2h' });
     res.send({ token });
+  }
+
+  static async getUserDetails(req, res, next){
+    const {id} = req.params;
+    try {
+      const user = await UserDatamapper.findByPk(id);
+      if(!user){
+        return next(new ApiError(`${this.entityName} not found`, {status: 404}));
+      }
+      const profiles = await ProfilDatamapper.findByUserId(id);
+      const posts = await PostDatamapper.findByUserId(id);
+      const rate = await RateDatamapper.getAllByUser(id);
+      return res.json({user, profiles, posts, rate});
+    } catch (error) {
+      console.error(error);
+      return next(new ApiError('Internal server error', {status: 500}));
+    }
+  }
+
+  static async getUserProfiles(req, res, next){
+    const {id} = req.params;
+    try {
+      const profiles = await ProfilDatamapper.findByUserId(id);
+      if(!profiles){
+        return next(new ApiError(`${this.entityName} npm`, {status: 404}));
+      }
+      return res.json({data: profiles});
+    } catch (error) {
+      console.error(error);
+      return next(new ApiError('Internal server error', {status: 500}));
+    }
   }
 }
--- app/datamappers/profil.datamapper.js
+++ app/datamappers/profil.datamapper.js
@@ -5,6 +5,13 @@ import CoreDatamapper from "./core.datamapper.js";
 
 export default class ProfilDatamapper extends CoreDatamapper {
   static tableName = 'profil';
+  static async findByUserId(userId) {
+    const result = await this.client.query(
+      `SELECT * FROM "${this.tableName}" WHERE "user_id" = $1`,
+      [userId],
+    );
+    return result.rows;
+  }
 }
 
 
